e799560b7f44d59f097ed0533a6585a8dacbbdff
Nico Lance Medina <nico@nlmedina.com>
2019-07-30 23:10:22 +0800

    Initial commit, ng create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'node-ly-client';
+  title = 'Node.ly';
+  name = 'Testing!';
 }
